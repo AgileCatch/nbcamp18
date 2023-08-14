84c58bf96ea169ad5d40791005caff18741a3149
jiyeon <cn0714@naver.com>
2023-08-14 22:09:44 +0900

    [DESIGN] : 🎨

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -2,15 +2,13 @@ package com.example.instagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_page_activity)
-
-        val ivcircle = findViewById<ImageView>(R.id.iv_circle)
-        ivcircle.clipToOutline = true
 
 
     }
