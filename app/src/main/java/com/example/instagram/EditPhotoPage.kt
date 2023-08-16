da35282eecde0aeba912444b7c872c43bb3b2695
AJH1346 <ahnzxght12@naver.com>
2023-08-16 19:19:06 +0900

    [DESIGN] : 🎨

---
+++ app/src/main/java/com/example/instagram/EditPhotoPage.kt
@@ -0,0 +1,11 @@
+package com.example.instagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPhotoPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_photo_page)
+    }
+}
---
+++ app/src/main/java/com/example/instagram/PhotoAlbumPage.kt
@@ -0,0 +1,11 @@
+package com.example.instagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoAlbumPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_album_page)
+    }
+}
