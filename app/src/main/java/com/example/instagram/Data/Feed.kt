b99e58b78a2d27ab2eca02df9d730488e81f6135
yeonghyeon <agilecatch@gmail.com>
2023-08-16 16:26:30 +0900

    [FEAT] : ⚙️
    디테일페이지 아이템 추가

--- app/src/main/java/com/example/instagram/Data/Feed.kt
+++ app/src/main/java/com/example/instagram/Data/Feed.kt
@@ -1,4 +1,4 @@
-package com.example.instagram
+package com.example.instagram.Data
 
 data class Feed (val profileImg:Int,
                  val profileName:String,
--- app/src/main/java/com/example/instagram/MainPageActivity.kt
+++ app/src/main/java/com/example/instagram/MainPageActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.instagram.Adapter.FeedAdapter
+import com.example.instagram.Data.Feed
 
 class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
