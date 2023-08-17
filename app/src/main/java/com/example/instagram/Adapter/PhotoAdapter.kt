b705464528579b46c47b5443d28f34506aab82d6
yeonghyeon <agilecatch@gmail.com>
2023-08-17 14:40:38 +0900

    수정중

--- app/src/main/java/com/example/instagram/Adapter/PhotoAdapter.kt
+++ app/src/main/java/com/example/instagram/Adapter/PhotoAdapter.kt
@@ -7,17 +7,17 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instagram.Data.Comment
+import com.example.instagram.Data.PhotoCard
 import com.example.instagram.Data.Userinfo
 import com.example.instagram.R
 
-class PhotoAdapter(var commentList:ArrayList<Comment>) : RecyclerView.Adapter<PhotoAdapter.CustomViewHolder>(){
+class PhotoCardAdapter(var photocardList:ArrayList<PhotoCard>) : RecyclerView.Adapter<PhotoCardAdapter.CustomViewHolder>(){
 
     class CustomViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val name = itemView.findViewById<TextView>(R.id.lv_name)
-        val nicname = itemView.findViewById<TextView>(R.id.lv_nicname)
-        val profileimg = itemView.findViewById<ImageView>(R.id.lv_profile)
-        val datetime = itemView.findViewById<TextView>(R.id.datetime)
-        val content =itemView.findViewById<TextView>(R.id.lv_comment)
+        val title = itemView.findViewById<TextView>(R.id.tv_title)
+        val photo = itemView.findViewById<ImageView>(R.id.iv_photo)
+        val content = itemView.findViewById<TextView>(R.id.tv_content)
+        val content_datetime =itemView.findViewById<TextView>(R.id.content_datetime)
 
     }
 
@@ -27,17 +27,16 @@ class PhotoAdapter(var commentList:ArrayList<Comment>) : RecyclerView.Adapter<Ph
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        holder.name.text=commentList.get(position).name
-        holder.nicname.text = commentList.get(position).nicname
-        holder.profileimg.setImageResource(commentList.get(position).profileimg)
-        holder.content.text = commentList.get(position).content
+        holder.title.text=photocardList.get(position).title//제목
+        holder.photo.setImageResource(photocardList.get(position).photo)
+        holder.content.text = photocardList.get(position).content//내용
 
-        val commentDate = commentList[position].datetime // 댓글의 날짜 데이터 가져오기
-        holder.datetime.text = commentDate // 날짜를 표시
+        val commentDate = photocardList[position].datetime // 사진의 날짜 데이터 가져오기
+        holder.content_datetime.text = commentDate // 날짜를 표시
     }
 
     override fun getItemCount(): Int {
-        return commentList.size
+        return photocardList.size
     }
 
 }
