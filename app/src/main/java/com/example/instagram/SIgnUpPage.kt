1feb88bf08481b8eb6fb094a68956426f6a14412
shyr0809 <139097952+shyr0809@users.noreply.github.com>
2023-08-17 17:59:32 +0900

    [FIX] : 🔧

--- app/src/main/java/com/example/instagram/SIgnUpPage.kt
+++ app/src/main/java/com/example/instagram/SIgnUpPage.kt
@@ -1,9 +1,11 @@
 package com.example.instagram
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.instagram.Data.Userinfo
 import com.example.instagram.Data.UserinfoSingleton
@@ -29,18 +31,21 @@ class SIgnUpPage : AppCompatActivity() {
             val name = newName.text.toString()
 
 
-            UserinfoSingleton.addUserinfo(Userinfo("추지연", "jiyeon", R.drawable.girl2,0, "안녕하세요\n추지연 미니홈피입니다.", 10, 900, R.drawable.miniroom2, "지연님의 미니룸"))
+            if (email.isBlank() || password.isBlank() || nickname.isBlank() || name.isBlank() ) {
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "회원가입 성공!", Toast.LENGTH_SHORT).show()
 
-            val signInPageIntent = Intent(this, SIgnInPage::class.java)
-            signInPageIntent.putExtra("email", email)
-            signInPageIntent.putExtra("password", password)
-            signInPageIntent.putExtra("nickname", nickname)
-            signInPageIntent.putExtra("name", name)
-            startActivity(signInPageIntent)
+                UserinfoSingleton.addUserinfo(Userinfo("추지연", "jiyeon", R.drawable.girl2,0, "안녕하세요\n추지연 미니홈피입니다.", 10, 900, R.drawable.miniroom2, "지연님의 미니룸"))
 
-
-            finish()
-
+                val signInPageIntent = Intent(this, SIgnInPage::class.java)
+                signInPageIntent.putExtra("email", email)
+                signInPageIntent.putExtra("password", password)
+                signInPageIntent.putExtra("nickname", nickname)
+                signInPageIntent.putExtra("name", name)
+                startActivity(signInPageIntent)
+                finish()
+            }
 
 
     }
