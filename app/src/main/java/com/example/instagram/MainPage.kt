a36fa13880dce12ba6271ece1d3eb7bb66a1340f
yeonghyeon <agilecatch@gmail.com>
2023-08-18 14:35:19 +0900

    [FEAT] : ⚙️ 리사이클러뷰 이미지추가
    로그인시 user정보가 mainpage 상단의 리사이클러뷰에 추가되도록 구현하였습니다.

--- app/src/main/java/com/example/instagram/MainPage.kt
+++ app/src/main/java/com/example/instagram/MainPage.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instagram.Adapter.FeedAdapter
+import com.example.instagram.Adapter.ProfileAdapter
 import com.example.instagram.Data.Userinfo
 import com.example.instagram.Data.UserinfoSingleton
 
@@ -29,36 +30,42 @@ class MainPage : AppCompatActivity() {
         }
         welcome.text = welcomeMessage
 
+        // 리사이클러뷰에 표시할 사용자 정보 가져오기
+        val userProfile = UserinfoSingleton.getUserinfoList()
+        val rvProfile = findViewById<RecyclerView>(R.id.rv_profile)
+        val profileAdapter = ProfileAdapter(userProfile)
 
-        //개인페이지 초기화
+        rvProfile.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvProfile.adapter = profileAdapter
+
+        //앨범에들어가는 사피
         UserinfoSingleton.addUserinfo(Userinfo("김영현", "yeonghyeon", R.drawable.girl1,0, "안녕하세요\n김영현 미니홈피입니다.", 100, 900, R.drawable.miniroom, "영현님의 미니룸"))
         UserinfoSingleton.addUserinfo(Userinfo("추지연", "jiyeon", R.drawable.girl2,0, "안녕하세요\n추지연 미니홈피입니다.", 10, 900, R.drawable.miniroom2, "지연님의 미니룸"))
         UserinfoSingleton.addUserinfo(Userinfo("이승현", "sh4340", R.drawable.man1,0, "안녕하세요\n이승현 미니홈피입니다.", 10, 900, R.drawable.miniroom3, "지연님의 미니룸"))
         UserinfoSingleton.addUserinfo(Userinfo("안주환", "ajh1346", R.drawable.man2,0, "안녕하세요\n안주환 미니홈피입니다.", 10, 900, R.drawable.miniroom5, "지연님의 미니룸"))
 
+//        //개인페이지 초기화값
+//        val ibProfile1 = findViewById<ImageButton>(R.id.ib_profile1)
+//        val ibProfile2 = findViewById<ImageButton>(R.id.ib_profile2)
+//        val ibProfile3 = findViewById<ImageButton>(R.id.ib_profile3)
+//        val ibProfile4 = findViewById<ImageButton>(R.id.ib_profile4)
+//        val userinfoList = UserinfoSingleton.getUserinfoList()
+//
+//        imageButtonChange("김영현", ibProfile1)
+//
+//        ibProfile1.setOnClickListener {
+//            val intent = Intent(this, MyPage::class.java)
+//            val userinfo = userinfoList[1]
+//
+//            UserinfoSingleton.todayIncrease(userinfo)
+//
+//            intent.putExtra("num", 0)
+//            startActivity(intent)
+//        }
+//
+//        moveDetailPage(ibProfile3,1)
 
-        val ibProfile1 = findViewById<ImageButton>(R.id.ib_profile1)
-        val ibProfile2 = findViewById<ImageButton>(R.id.ib_profile2)
-        val ibProfile3 = findViewById<ImageButton>(R.id.ib_profile3)
-        val ibProfile4 = findViewById<ImageButton>(R.id.ib_profile4)
-
-
-        val userinfoList = UserinfoSingleton.getUserinfoList()
-
-        imageButtonChange("김영현", ibProfile1)
-
-        ibProfile1.setOnClickListener {
-            val intent = Intent(this, MyPage::class.java)
-            val userinfo = userinfoList[0]
-
-            UserinfoSingleton.todayIncrease(userinfo)
-
-            intent.putExtra("num", 0)
-            startActivity(intent)
-        }
-
-        moveDetailPage(ibProfile3,1)
-
+        //피드보여주기
         val feedList = arrayListOf(
             //수정 필요
             userinfoList.find{it.name == "김영현"} as Userinfo,
--- app/src/main/java/com/example/instagram/SIgnUpPage.kt
+++ app/src/main/java/com/example/instagram/SIgnUpPage.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.instagram.Data.Userinfo
 import com.example.instagram.Data.UserinfoSingleton
 
+
 class SIgnUpPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,14 +40,11 @@ class SIgnUpPage : AppCompatActivity() {
 
 
             // 새로운 사용자 정보 생성
-            val newUserinfo = Userinfo(name, nickname, R.drawable.newprofile, 0, "", 0, 0, 0, "")
-
-            // 생성한 사용자 정보를 UserinfoSingleton에 추가
-            UserinfoSingleton.addUserinfo(newUserinfo)
+            val newUserinfo = Userinfo(name, nickname, R.drawable.newprofile, 0, "", 0, 0, R.drawable.empty1, "")
+                // 생성한 사용자 정보를 UserinfoSingleton에 추가
+                UserinfoSingleton.addUserinfo(newUserinfo)
 
             //다음화면으로 이동하는 코드
-
-
                 val signInPageIntent = Intent(this, SIgnInPage::class.java)
                 signInPageIntent.putExtra("email", email)
                 signInPageIntent.putExtra("password", password)
