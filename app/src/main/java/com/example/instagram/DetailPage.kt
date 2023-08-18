6dc667c6745622ddc86223ca5001301a7965fff2
jiyeon <cn0714@naver.com>
2023-08-18 18:12:04 +0900

    [FEATURE] : 데이터 연동, MainPage 이미지 연결

--- app/src/main/java/com/example/instagram/DetailPage.kt
+++ app/src/main/java/com/example/instagram/DetailPage.kt
@@ -16,14 +16,6 @@ import com.example.instagram.Data.Comment
 import com.example.instagram.Data.UserinfoSingleton
 
 
-//댓글 초기화
-val commentList = arrayListOf(
-    Comment("김영현", "관심일촌", R.drawable.girl1, "2023.08.16", "18조 화이팅!"),
-    Comment("추지연", "어느별에서왔니도대체", R.drawable.girl2, "2023.08.15", "■■■■■□90% 충전중"),
-    Comment("이승현", "우린 먼가 달라", R.drawable.man1, "2023.08.14", "거기 하늘라이프죠?"),
-    Comment("안주환", "너는나의엔돌핀", R.drawable.man2, "2023.08.13", "Very important person"),
-)
-
 class DetailPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -105,7 +97,7 @@ class DetailPage : AppCompatActivity() {
                         Comment(
                             "작성자 이름",
                             ilchonText,
-                            R.drawable.girl2,
+                            R.drawable.man1,
                             getCurrentDate(),
                             contentText
                         )
